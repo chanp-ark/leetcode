cb2f613285327e706c13a0717057e2aa5fd5c019
Chan Park <chanpark93@gmail.com>
2020-05-13 15:54:13 -0700

    refactor to remove unecessary variables

--- palindromeInteger.js
+++ palindromeInteger.js
@@ -5,9 +5,7 @@ const palindromeInteger = num => {
     const mid = Math.floor(numToStr.length/2)
 
     if (numToStr.length %2 === 0 && num %11 === 0) {
-        const former = numToStr.slice(0, mid)
-        const latter = numToStr.slice(mid)
-        if (former[0] === latter[latter.length-1]) return true
+        if (numToStr[0] === numToStr[numToStr.length-1]) return true
        
     } else {
         const latter = numToStr.slice(mid+1).split('').reverse().join('')
@@ -17,8 +15,9 @@ const palindromeInteger = num => {
 }
 
 
-console.log("should be true: ",palindromeInteger(1221))
-console.log("should be false: ",palindromeInteger(-121))
-console.log("should be true: ",palindromeInteger(95859))
-console.log("should be false: ",palindromeInteger(1133))
-console.log("should be false: ",palindromeInteger(123123))
+console.log("should be true: ", palindromeInteger(1221))
+console.log("should be false: ", palindromeInteger(-121))
+console.log("should be false for 10: ", palindromeInteger(10))
+console.log("should be true: ", palindromeInteger(95859))
+console.log("should be false: ", palindromeInteger(1133))
+console.log("should be false: ", palindromeInteger(123123))
