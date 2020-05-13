c5dc686523e9b016a91367acbbb81473dad9d123
Chan Park <chanpark93@gmail.com>
2020-05-13 15:29:30 -0700

    Determine whether an integer is a palindrome

---
+++ palindromeInteger.js
@@ -0,0 +1,24 @@
+const palindromeInteger = num => {
+    if (num < 0) return false
+    if (num < 10) return true
+    const numToStr = num.toString()
+    const mid = Math.floor(numToStr.length/2)
+
+    if (numToStr.length %2 === 0 && num %11 === 0) {
+        const former = numToStr.slice(0, mid)
+        const latter = numToStr.slice(mid)
+        if (former[0] === latter[latter.length-1]) return true
+       
+    } else {
+        const latter = numToStr.slice(mid+1).split('').reverse().join('')
+        if (numToStr.slice(0, mid) === latter) return true
+    }
+    return false
+}
+
+
+console.log("should be true: ",palindromeInteger(1221))
+console.log("should be false: ",palindromeInteger(-121))
+console.log("should be true: ",palindromeInteger(95859))
+console.log("should be false: ",palindromeInteger(1133))
+console.log("should be false: ",palindromeInteger(123123))
