3c09635bd39ce9451c1a5182e8c5a0c983bbe1b4
chanp-ark <chanpark93@gmail.com>
2020-05-29 16:14:05 -0700

    Implement strStr(). Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.

---
+++ strStr.js
@@ -0,0 +1,22 @@
+// Return the index of the first occurrence of needle in haystack, 
+// or -1 if needle is not part of haystack.
+
+// return 0 when needle is an empty string. 
+// This is consistent to C's strstr() and Java's indexOf()
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+const strStr = (haystack, needle) => {
+    if (!needle) return 0
+    let end = needle.length
+    for (let i = 0; i < haystack.length; i++) {
+        const check = haystack.slice(i, i+end)
+        if (check === needle) return i
+    }
+    return -1
+};
+
