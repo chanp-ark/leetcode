6e23ca514a7302cc64ffb8c0fcf0be12b6fe5618
chanp-ark <chanpark93@gmail.com>
2020-05-29 16:32:13 -0700

    Implement strStr(). Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.

--- strStr.js
+++ strStr.js
@@ -16,7 +16,11 @@ const strStr = (haystack, needle) => {
     for (let i = 0; i < haystack.length; i++) {
         const check = haystack.slice(i, i+end)
         if (check === needle) return i
+        if (check.length !== needle.length) return -1
+
     }
     return -1
-};
+}
 
+// one liner, works for array or string
+const strStr1 = (haystack, needle) => haystack.indexOf(needle);
